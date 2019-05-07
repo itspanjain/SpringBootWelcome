e17aba038281b7a7318615cf95bcf824ac850566
Pankaj <jainpankaj_15m@yahoo.com>
2019-05-08 00:01:38 +0530

    Spring Security added with Basic Authentication

--- src/main/java/com/demo/SpringBootWelcome/Controllers/WelcomeController.java
+++ src/main/java/com/demo/SpringBootWelcome/Controllers/WelcomeController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+
+    @RequestMapping("/")
+    public String greeting(){
+        return "Greetings from SpringBootWelcome Application!!!";
+    }
     @RequestMapping("/welcome")
     public String welcome(){
         return "Welcome User!!!";
